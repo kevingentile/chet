b18c21fd6fa6c6a7dc72a8289cb1137e6a5bf07f
Kevin Gentile <kgentile@protonmail.com>
2022-01-03 22:57:08 -0500

    rework for changestream startAfter

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"log"
 	"time"
@@ -60,47 +61,6 @@ func main() {
 	database := client.Database("chet")
 	streamCollection := database.Collection("chet-stream")
 
-	changeStream, err := streamCollection.Watch(context.TODO(), mongo.Pipeline{})
-	if err != nil {
-		panic(err)
-	}
-
-	defer changeStream.Close(context.TODO())
-
-	lastLease := &Lease{}
-	auxCollection := database.Collection("chet-aux")
-	opt := options.FindOne()
-	opt.SetSort(bson.M{"$natural": -1})
-	if err := auxCollection.FindOne(context.TODO(), bson.M{}, opt).Decode(lastLease); err != nil {
-		panic(err)
-	}
-
-	fmt.Println("lastLease", lastLease)
-	if *lastLease == (Lease{}) {
-		lease := Lease{
-			ID:        "main-" + uuid.NewString(),
-			ExpiresAt: time.Now().Add(time.Minute * 5),
-			StartAt:   time.Now(),
-		}
-		fmt.Println("Acquiring lease..", lease)
-		if _, err := auxCollection.InsertOne(context.TODO(), lease); err != nil {
-			panic(err)
-		}
-	}
-	runner := func() error {
-		if changeStream.Next(context.TODO()) {
-			var data ChangeEvent
-
-			if err := changeStream.Decode(&data); err != nil {
-				return err
-			}
-			fmt.Println("data:", data.FullDocument)
-		}
-		return nil
-	}
-
-	deadline := time.Now().Add(time.Second * 10)
-
 	go func() {
 		for {
 			msg := Message{
@@ -113,10 +73,85 @@ func main() {
 				log.Println(err)
 			}
 			streamCollection.InsertOne(context.TODO(), m)
-			time.Sleep(1 * time.Second)
+			time.Sleep(20 * time.Second)
 		}
 	}()
-	runUntil(deadline, runner)
+
+	leaseID := "main-" + uuid.NewString()
+	for {
+		// check for an existing lease
+		log.Println("checking lease...")
+		lastLease := &Lease{}
+		auxCollection := database.Collection("chet-aux")
+		opt := options.FindOne()
+		opt.SetSort(bson.M{"$natural": -1})
+		if err := auxCollection.FindOne(context.TODO(), bson.M{}, opt).Decode(lastLease); err != nil {
+			if !errors.Is(err, mongo.ErrNoDocuments) {
+				panic(err)
+			}
+		}
+
+		// create a new lease
+		lease := Lease{
+			ID:        leaseID,
+			ExpiresAt: time.Now().Add(time.Minute * 1),
+			StartAt:   time.Now(),
+		}
+		// if no lease exists, acquire the first lease
+		// TODO service startup race may insert multiples
+		if *lastLease == (Lease{}) {
+			log.Println("acquiring initial lease..", lease)
+			if _, err := auxCollection.InsertOne(context.TODO(), lease); err != nil {
+				panic(err)
+			}
+		} else {
+			// if the previous lease has expired, attempt to acquire a new lease
+			if lastLease.ExpiresAt.Before(time.Now()) {
+				filter := bson.M{"id": lastLease.ID}
+				log.Println("acquiring lease...", lease)
+				r, err := auxCollection.ReplaceOne(context.TODO(), filter, lease)
+				if err != nil {
+					panic(err)
+				}
+				if r.MatchedCount > 1 {
+					panic(errors.New("ERROR: lease replacement matches multiple results"))
+				}
+			}
+		}
+
+		// verify
+		checkLease := &Lease{}
+		if err := auxCollection.FindOne(context.TODO(), bson.M{}, opt).Decode(checkLease); err != nil {
+			panic(err)
+		}
+
+		if checkLease.ID != lease.ID {
+			log.Println("lease not owned", *checkLease)
+		} else {
+			log.Println("acquired lease", checkLease.ID)
+			copts := options.ChangeStream()
+			copts.SetStartAtOperationTime(&primitive.Timestamp{T: uint32(checkLease.StartAt.Unix())})
+			changeStream, err := streamCollection.Watch(context.TODO(), mongo.Pipeline{}, copts)
+			if err != nil {
+				panic(err)
+			}
+
+			runner := func() error {
+				if changeStream.Next(context.TODO()) {
+					var data ChangeEvent
+
+					if err := changeStream.Decode(&data); err != nil {
+						return err
+					}
+					log.Println("data:", data.ID, data.FullDocument)
+				}
+				return nil
+			}
+			runUntil(checkLease.ExpiresAt, runner)
+			changeStream.Close(context.TODO())
+		}
+		time.Sleep(time.Until(checkLease.ExpiresAt))
+	}
 }
 
 func runUntil(deadline time.Time, runner func() error) {
